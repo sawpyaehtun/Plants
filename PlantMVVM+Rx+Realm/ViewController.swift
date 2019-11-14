034d8e0f861424421a47252ba4df9adcd1e72d8f
sawpyaehtun <sawpyaehtun@gmail.com>
2019-11-14 20:41:50 +0630

    Initial Commit

---
+++ PlantMVVM+Rx+Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlantMVVM+Rx+Realm
+//
+//  Created by sawpyaehtun on 14/11/2019.
+//  Copyright © 2019 sawpyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlantMVVM+Rx+RealmTests/PlantMVVM_Rx_RealmTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlantMVVM_Rx_RealmTests.swift
+//  PlantMVVM+Rx+RealmTests
+//
+//  Created by sawpyaehtun on 14/11/2019.
+//  Copyright © 2019 sawpyaehtun. All rights reserved.
+//
+
+import XCTest
+@testable import PlantMVVM_Rx_Realm
+
+class PlantMVVM_Rx_RealmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
